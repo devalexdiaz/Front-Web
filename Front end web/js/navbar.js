2221cdd4ca31a5e4f45df1ac013175d856bb26d3
Alexander Diaz <98404378+devalexdiaz@users.noreply.github.com>
2023-05-25 20:11:25 -0400

    Navbar

---
+++ Front end web/js/navbar.js
@@ -0,0 +1,12 @@
+// Funciones Javascript para abrir y cerrar el navbar de modo movil
+
+function openNav(){
+    document.getElementById("menu-movil").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("menu-movil").style.width = "0%";
+}
+
+// Función Javascript ...
+
